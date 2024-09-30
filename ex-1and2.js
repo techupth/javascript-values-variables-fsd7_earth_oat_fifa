a8e33393a63f0382bb010bbf95bd30ee47368657
Oat Chana <chanathip0944729842@gmail.com>
2024-09-30 13:41:21 +0700

    feat:add answer ex1,2,3,4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,12 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let name = "Karter";
+let surname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const resterantName = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const activeRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+totalPrice = 5000
+activeRestaurant = 2500

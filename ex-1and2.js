d7c21d1fd308f6d858640cad918c97c8b5ac1d47
Thanakon <thanakon.piti@gmail.com>
2024-09-30 13:32:28 +0700

    feat: variable and value on ex1-2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let firstname = "Karter";
+let lastname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const nameRestaurant = "Chef's Table by Chef James";
+const netPriceBill = 15000;
+const totalRestaurantOpen = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netPriceBill = 5000;
+totalRestaurantOpen = 2500;
